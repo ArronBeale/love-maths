17f914950bf0a31897344b0ad8391909dd190714
Arron <beale86@msn.com>
2022-03-28 21:28:52 +0000

    Adjust division answer to always be an integer

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,7 +135,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         function displayDivideQuestion(operand1, operand2) {
-            document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-            document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+            document.getElementById("operand1").textContent = operand1 * operand2;
+            document.getElementById("operand2").textContent = operand2;
             document.getElementById("operator").textContent = "÷";
         }
