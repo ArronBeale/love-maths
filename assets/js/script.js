00fa1b0d216a7919995bd59629292f415906731d
Arron <beale86@msn.com>
2022-03-28 19:36:58 +0000

    Add favicon and final tidy up for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Wait for DOM to finish loading before running the game
 // Get the Button elements and add event listeners
-
 document.addEventListener("DOMContentLoaded", function() {
+
     let buttons = document.getElementsByTagName("button");
 
         for (let button of buttons) {
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
               });
           }
 
+          document.getElementById("answer-box").addEventListener("keydown", function(event) {
+              if (event.key === "Enter") {
+                  checkAnswer();
+              }
+          })
+
           runGame("addition");
 
      });
@@ -23,6 +29,10 @@ document.addEventListener("DOMContentLoaded", function() {
       * and after the user's answer has been processed
       */
         function runGame(gameType) {
+
+            document.getElementById("answer-box").value = "";
+            document.getElementById("answer-box").focus();
+
             // Creates two random numbers between 1 and 25
             let num1 = Math.floor(Math.random() * 25) + 1;
             let num2 = Math.floor(Math.random() * 25) + 1;
@@ -44,6 +54,7 @@ document.addEventListener("DOMContentLoaded", function() {
          * returned calculatedAnswer array
          */
         function checkAnswer() {
+
             let userAnswer = parseInt(document.getElementById("answer-box").value);
             let calculatedAnswer = calculateCorrectAnswer();
             let isCorrect = userAnswer === calculatedAnswer[0];
@@ -63,6 +74,7 @@ document.addEventListener("DOMContentLoaded", function() {
          * directly from the dom and returns the correct answer.
          */
         function calculateCorrectAnswer() {
+
             let operand1 = parseInt(document.getElementById('operand1').innerText);
             let operand2 = parseInt(document.getElementById('operand2').innerText);
             let operator = document.getElementById('operator').innerText;
@@ -83,6 +95,7 @@ document.addEventListener("DOMContentLoaded", function() {
          * Gets the current score from the DOM and increments it by 1
          */
         function incrementScore() {
+
             let oldScore = document.getElementById("score").innerText;
             document.getElementById("score").innerText = ++oldScore;
         }
@@ -91,23 +104,27 @@ document.addEventListener("DOMContentLoaded", function() {
          * Gets the current sum of incorrect answers from the DOM and increments by 1
          */
         function incrementWrongAnswer() {
+
             let oldScore = document.getElementById("incorrect").innerText;
             document.getElementById("incorrect").innerText = ++oldScore;
         }
 
         function displayAdditionQuestion(operand1, operand2) {
+
             document.getElementById('operand1').textContent = operand1;
             document.getElementById('operand2').textContent = operand2;
             document.getElementById('operator').textContent = "+";
         }
 
         function displaySubtractQuestion(operand1, operand2) {
+
             document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
             document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
             document.getElementById("operator").textContent = "-";
         }
 
         function displayMultiplyQuestion(operand1, operand2) {
+
             document.getElementById('operand1').textContent = operand1;
             document.getElementById('operand2').textContent = operand2;
             document.getElementById('operator').textContent = "x";
